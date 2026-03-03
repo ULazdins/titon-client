704e313251082f554b4e66946731c79999c548fb
Ugis Lazdins <ugis.lazdins@gmail.com>
2026-03-03 15:02:00 +0200

    Add more sensors

--- TitonGeneralInfo.py
+++ TitonGeneralInfo.py
@@ -15,6 +15,11 @@ class TitonGeneralInfo:
             f":DAT|{self.client.hrv_mac}|{self.client.my_mac}|PS|<stx>L"
         )
 
+        # parsed flags
+        self.filter_flag = None
+        self.frost_flag = None
+        self.summer_flag = None
+
     async def perform(self):
         response = await self.client.send_request_response(
             self.message,
@@ -32,24 +37,38 @@ class TitonGeneralInfo:
         bits2 = split_into_bits(int(byte2, 16))
         bits3 = split_into_bits(int(byte3, 16))
 
-        # FunModel
-        _LOGGER.debug(f"Byte1 {bits1}")
-        _LOGGER.debug(f"Speed1 {bits1[0]}")
-        _LOGGER.debug(f"Speed2 {bits1[1]}")
-        _LOGGER.debug(f"Speed3 {bits1[2]}")
-        _LOGGER.debug(f"Speed4 {bits1[3]}")
-        _LOGGER.debug(f"Timer {bits1[4]}")
-        _LOGGER.debug(f"Switch {bits1[5]}")
+        # FunModel bits (Byte1)
+        _LOGGER.debug("Byte1 %s", bits1)
+        _LOGGER.debug("Speed1 %s", bits1[0])
+        _LOGGER.debug("Speed2 %s", bits1[1])
+        _LOGGER.debug("Speed3 %s", bits1[2])
+        _LOGGER.debug("Speed4 %s", bits1[3])
+        _LOGGER.debug("Timer %s", bits1[4])
+        _LOGGER.debug("Switch %s", bits1[5])
 
-        _LOGGER.debug(f"\nByte2 {bits2}")
-        _LOGGER.debug(f"Sensor {bits2[0]}")
-        _LOGGER.debug(f"Filter {bits2[1]}")
-        _LOGGER.debug(f"Inhibit {bits2[2]}")
-        _LOGGER.debug(f"Boost {bits2[3]}")
-        _LOGGER.debug(f"Frost {bits2[4]}")
-        _LOGGER.debug(f"Internal {bits2[5]}")
+        # Byte2 contains status flags such as Filter, Frost, Boost
+        _LOGGER.debug("\nByte2 %s", bits2)
+        _LOGGER.debug("Sensor %s", bits2[0])
+        _LOGGER.debug("Filter %s", bits2[1])
+        _LOGGER.debug("Inhibit %s", bits2[2])
+        _LOGGER.debug("Boost %s", bits2[3])
+        _LOGGER.debug("Frost %s", bits2[4])
+        _LOGGER.debug("Internal %s", bits2[5])
 
-        _LOGGER.debug(f"\nByte3 {bits3}")
-        _LOGGER.debug(f"Summer {bits3[0]}")
-        _LOGGER.debug(f"Attention {bits3[1]}")
-        _LOGGER.debug(f"Duct heater {bits3[2]}")
+        # store parsed flags for external use
+        self.filter_flag = bool(bits2[1])
+        self.frost_flag = bool(bits2[4])
+
+        # Byte3 contains summer and other flags
+        _LOGGER.debug("\nByte3 %s", bits3)
+        _LOGGER.debug("Summer %s", bits3[0])
+        _LOGGER.debug("Attention %s", bits3[1])
+        _LOGGER.debug("Duct heater %s", bits3[2])
+
+        self.summer_flag = bool(bits3[0])
+
+        return {
+            "filter": self.filter_flag,
+            "frost": self.frost_flag,
+            "summer": self.summer_flag,
+        }
---
+++ __init__.py
@@ -0,0 +1,12 @@
+from .TitonFilter import TitonFilter
+from .TitonHumidity import TitonHumidity
+from .TitonSummer import TitonSummer
+
+__all__ = [
+	"TitonClient",
+	"TitonFanSpeed",
+	"TitonGeneralInfo",
+	"TitonFilter",
+	"TitonHumidity",
+	"TitonSummer",
+]
